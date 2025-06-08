1d471c34a829998703d181e775c55d9fcf3f65f6
wangfq <1415405435@qq.com>
2025-06-08 14:35:23 +0800

    feat: 增加shadcn 组件库

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div> 
-      <p className="font-bold text-rose-500">
-        hellow word!
-      </p>
-    </div>
+   <Button>Click on</Button>
   );
 }
