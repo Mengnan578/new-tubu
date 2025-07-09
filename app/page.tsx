7c5e6ae72b632e498784b049470cda4f5fc46dd9
wangfq <1415405435@qq.com>
2025-07-09 16:00:08 +0800

    fix: 将.dea 添加到.gitignore 中去

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,11 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-   <Button>Click on</Button>
+    <>
+      <Button>Click on</Button>
+      <div>
+        <p className="font-bold text-rose-400">hellow world</p>
+      </div>
+    </>
   );
 }
