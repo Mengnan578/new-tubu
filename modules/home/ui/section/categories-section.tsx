b11213762d054508ede52ba0e1fdf31cb7327bc6
wangfq <1415405435@qq.com>
2025-07-26 00:32:18 +0800

    feat(home): 添加 Suspense 和 ErrorBoundary 包装分类组件
    为分类组件添加 Suspense 和 ErrorBoundary 包装，提升用户体验和错误处理能力

--- modules/home/ui/section/categories-section.tsx
+++ modules/home/ui/section/categories-section.tsx
@@ -1,19 +1,29 @@
 "use client";
 
 import { trpc } from "@/trpc/client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 interface CategoriesSectionProps {
   categoryId?: string;
 }
 
 export const CategoriesSection = ({ categoryId }: CategoriesSectionProps) => {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <ErrorBoundary
+        fallbackRender={({ error }) => <p>Error: {error.message}</p>}
+      >
+        <CategoriesSectionSuspense categoryId={categoryId} />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+const CategoriesSectionSuspense = ({
+  categoryId,
+}: CategoriesSectionProps) => {
   const [categories] = trpc.category.getMany.useSuspenseQuery();
   console.log(categoryId);
-  return (
-    <div>
-      {
-        JSON.stringify(categories)
-      }
-    </div>
-  );
+  return <div>{JSON.stringify(categories)}</div>;
 };
