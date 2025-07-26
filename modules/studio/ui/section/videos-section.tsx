8b54d3ead409c424278cbf5f9f9da4d63176134d
wangfq <1415405435@qq.com>
2025-07-26 23:48:07 +0800

    feat(studio): 添加频道内容页面和视频表格展示
    为频道内容页面添加标题和描述，并实现视频数据的表格展示
    使用Suspense和ErrorBoundary优化加载体验
    添加无限滚动功能以支持分页加载

--- modules/studio/ui/section/videos-section.tsx
+++ modules/studio/ui/section/videos-section.tsx
@@ -1,19 +1,83 @@
-'use client'
+"use client";
 
 import { trpc } from "@/trpc/client";
 import { DEFAULT_LIMIT } from "@/lib/constants";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { InfiniteScroll } from "@/components/infinite-scroll";
+import {
+  TableHead,
+  TableHeader,
+  Table,
+  TableRow,
+  TableBody,
+  TableCell,
+} from "@/components/ui/table";
+import { useRouter } from "next/navigation";
 
 export const VideosSection = () => {
-    const [data] = trpc.studio.getMany.useSuspenseInfiniteQuery({
-        limit: DEFAULT_LIMIT,
-    },{ 
-        getNextPageParam: (lastPage) => lastPage.nextCursor,
-    });
-    return (
-        <div>
-            {
-                JSON.stringify(data)
-            }
-        </div>
-    )
-}
+  return (
+    <Suspense fallback={<div>loading</div>}>
+      <ErrorBoundary fallback={<div>error</div>}>
+        <VideosSectionSuspense />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+export const VideosSectionSuspense = () => {
+  const [videos, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
+    {
+      limit: DEFAULT_LIMIT,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    }
+  );
+
+  const router = useRouter();
+  return (
+    <div>
+      <div className="border-y">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="pl-6 w-[510px]">Video</TableHead>
+              <TableHead>Visibility</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="text-right">Views</TableHead>
+              <TableHead className="text-right">Comments</TableHead>
+              <TableHead className="text-right pr-6">likes</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {videos.pages
+              .flatMap((page) => page.items)
+              .map((video) => (
+                <TableRow
+                  key={video.id}
+                  className="cursor-pointer"
+                  onClick={() => router.push(`/studio/video/${video.id}`)}
+                >
+                  <TableCell>{video.title}</TableCell>
+                  <TableCell>visibility</TableCell>
+                  <TableCell>status</TableCell>
+                  <TableCell>Date</TableCell>
+                  <TableCell className="text-right">views</TableCell>
+                  <TableCell className="text-right">comments</TableCell>
+                  <TableCell className="text-right pr-6">likes</TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </div>
+      <InfiniteScroll
+        isManual
+        hasNextPage={query.hasNextPage}
+        isFetchingNextPage={query.isFetchingNextPage}
+        fetchNextPage={query.fetchNextPage}
+      />
+    </div>
+  );
+};
